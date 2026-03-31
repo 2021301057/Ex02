dd9d4b332ad76b5c8f6d210fc5eecb519cb6f64e
2021301057 <dbsgustlr77@skuniv.ac.kr>
2026-03-31 16:05:33 +0900

    16:05

--- src/main/java/pack/Ex02Application.java
+++ src/main/java/pack/Ex02Application.java
@@ -2,9 +2,17 @@ package pack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Ex02Application {
+	@GetMapping("/")
+	public String hello() {
+		System.out.println("Ex02");
+		return "Ex02 Project: 16:05";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Ex02Application.class, args);
